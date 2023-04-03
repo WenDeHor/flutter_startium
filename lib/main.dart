8123b30741a74bc378aa083995b8cfb223a4fc88
WenDeHor <mazur23252@gmail.com>
2023-04-03 09:57:16 +0300

    2.Button stiles: icon, text, color, form

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,31 +9,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.deepOrangeAccent), //Not working
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Test app by progect'),
-              backgroundColor: Colors.deepOrangeAccent,
-              centerTitle: true,
-            ),
-            body: Center(
-              child: Text(
-                'Text of my body',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.red,
-                    fontFamily: 'DeliciousHandrawn'
-                ),
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              child: Text('Press'),
-              backgroundColor: Colors.deepOrangeAccent,
-              onPressed: (){
-                print('press');
-              },
-            ),
+      theme: ThemeData(primaryColor: Colors.deepOrangeAccent), //Not working
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Test app by progect'),
+          backgroundColor: Colors.deepOrangeAccent,
+          centerTitle: true,
         ),
+        body: Center(
+          child: ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.supervised_user_circle_rounded,
+              color: Colors.deepOrangeAccent,
+            ),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(106, 194, 252, 50.0)),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder( borderRadius: BorderRadius.circular(10) ))
+            ),
+            label: const Text('Press me'),
+          ),
+//          child: Icon(
+//            Icons.supervised_user_circle_rounded,
+//            size: 45,
+//            color: Colors.deepOrangeAccent,
+//          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepOrangeAccent,
+          onPressed: () {
+            print('press');
+          },
+          child: const Text('Press'),
+        ),
+      ),
     );
   }
 }
